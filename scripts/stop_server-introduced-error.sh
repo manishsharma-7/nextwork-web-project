64f97244cfc17c7a2256489ca2c8419e9fab7be1
EC2 Default User <ec2-user@ip-172-31-13-191.ap-southeast-2.compute.internal>
2025-07-21 07:27:42 +0000

    update file mode chmod +x

--- scripts/stop_server-introduced-error.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Add this at the top of your script to make it exit immediately if any command fails
-set -e
-
-# Your intentional error
-sudo systemctll stop httpd.service  # Misspelled command will now cause script to exit with non-zero status
-
-# To be extra sure, we can also add an explicit exit code
-exit 1
